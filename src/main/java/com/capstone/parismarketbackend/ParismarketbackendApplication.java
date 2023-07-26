6b115f9c02ed406d9aaa521c0a841e1e5b0c0bd4
Zhuo <zhuo.parse@gmail.com>
2023-07-25 19:29:43 -0700

    added dependency

---
+++ src/main/java/com/capstone/parismarketbackend/ParismarketbackendApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.parismarketbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParismarketbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParismarketbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capstone/parismarketbackend/ParismarketbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.parismarketbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParismarketbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
